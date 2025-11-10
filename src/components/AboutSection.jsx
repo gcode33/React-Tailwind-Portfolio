373f5939d95d30e631d320b85b2dbad760f12872
George Fotabong <77421099+gcode33@users.noreply.github.com>
2025-11-09 19:28:34 -0500

    changes to the resume and about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,7 +27,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="/GeorgeFotabong.pdf"
+                href="/GeorgeFotabongCV.pdf"
                 download="GeorgeFotabong_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
