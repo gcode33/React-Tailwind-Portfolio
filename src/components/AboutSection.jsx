b5c22964b7c967fd9303c4f5f1ceb8e7a9118db2
George Fotabong <77421099+gcode33@users.noreply.github.com>
2025-12-12 14:34:51 -0500

    fix to file path

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,7 +27,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="C:\Users\georg\Portforlio_Website\public\GeorgeFotabongJr_CV.pdf"
+                href="/GeorgeFotabongJr_CV.pdf"
                 download="GeorgeFotabongJr_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
